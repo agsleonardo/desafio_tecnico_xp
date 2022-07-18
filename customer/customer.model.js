9f846c3667be88b830f26e671749bda1a1ad037e
agsleonardo <agsleonardo@hotmail.com>
2022-07-18 16:25:12 -0300

    feat:

--- customer/customer.model.js
+++ customer/customer.model.js
@@ -1,24 +1,43 @@
+const fs = require('fs');
+const path = require('path');
+
 const mysql = require('mysql2/promise');
+
 require('dotenv').config();
 
 const connection = mysql.createPool({
   host: process.env.MYSQL_HOST || 'localhost',
   user: process.env.MYSQL_USER || 'root',
-  password: process.env.MYSQL_PASSWORD || 'password',
-  database: process.env.MYSQL_DATABASE || 'customer',
+  password: process.env.MYSQL_PASSWORD || 'root',
 });
 
+const recreateDatabase = async () => {
+  try {
+    const importPath = path.resolve(__dirname, 'customer.sql');
+    const seedDBContent = fs.readFileSync(importPath).toString();
+    const queries = seedDBContent.split(';').filter((p) => p.trim());
+    const inserts = queries.pop();
+    const queriesPromises = queries.map(async (query) => connection.execute(query));
+    await Promise.all(queriesPromises);
+    await connection.execute(inserts);
+    process.stdout.write('\nDatabase inciado...\n');
+  } catch (error) {
+    process.stdout.write(`Falha em restaurar o Banco. ${error}`);
+  }
+};
+
 const getById = async (id) => {
-  const [[rows]] = await connection.query('SELECT * FROM customer WHERE id = ?', [id]);
+  const [[rows]] = await connection.query('SELECT * FROM Customer_db.Customer WHERE id = ?', [id]);
   return rows;
 };
 
 const create = async ({ email, username, password }) => {
-  const [[rows]] = await connection.query('INSERT INTO customer (email, username, password) VALUES (?, ?, ?)', [email, username, password]);
-  return rows;
+  const [{ insertId }] = await connection.query('INSERT INTO Customer_db.Customer (email, username, password) VALUES (?, ?, ?)', [email, username, password]);
+  return insertId;
 };
 
 module.exports = {
   getById,
   create,
+  recreateDatabase,
 };
--- customer/customer.routes.js
+++ customer/customer.routes.js
@@ -3,6 +3,8 @@ const customer = require('./customer');
 
 const routes = express.Router();
 
+routes.get('/', (req, res) => res.send('Esse é o serviço de clientes'));
+
 routes.post('/', customer.create);
 
 routes.get('/:id', customer.getById);
