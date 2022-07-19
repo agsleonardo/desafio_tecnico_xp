22f41f72d425761fe3d35725f1064bdec69936af
agsleonardo <agsleonardo@hotmail.com>
2022-07-19 15:53:43 -0300

    feat:

--- accounts/accounts.routes.js
+++ accounts/accounts.routes.js
@@ -1,6 +1,12 @@
 const router = require('express').Router();
+
+const validates = require('./middlewares/validateRequest');
 const accounts = require('./accounts');
 
 router.get('/:accountId', accounts.getByAccountId);
+
+router.put('/withdraw', validates.validateWithdrawRequest, accounts.withdraw);
+
+// router.get('/deposit', accounts.getByAccountId);
 
 module.exports = router;
