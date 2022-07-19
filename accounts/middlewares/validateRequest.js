05e18ec4fab69aacc3666b516d2585df5a6b6bf4
agsleonardo <agsleonardo@hotmail.com>
2022-07-19 16:07:53 -0300

    refactor: refatorando método para atender à ambas operações

--- accounts/middlewares/validateRequest.js
+++ accounts/middlewares/validateRequest.js
@@ -1,12 +1,12 @@
 const customError = require('../utils/error');
 
-const validateWithdrawRequest = (req, _res, next) => {
+const validateAccountRequest = (req, _res, next) => {
   const { accountId, amount } = req.body;
   if (!accountId || !amount) throw customError(400, 'Dados incompletos');
-  if (+amount <= 0) throw customError(400, 'A quantia para saque deve ser maior que zero');
+  if (+amount <= 0) throw customError(400, 'A quantia para operação deve ser maior que zero');
   next();
 };
 
 module.exports = {
-  validateWithdrawRequest,
+  validateAccountRequest,
 };
