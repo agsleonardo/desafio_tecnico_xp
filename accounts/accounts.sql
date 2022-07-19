ef8a33cd6f308b5df380fa9e428f68f23fb3280d
agsleonardo <agsleonardo@hotmail.com>
2022-07-19 12:56:09 -0300

    feat: criando carga inicial accounts

---
+++ accounts/accounts.sql
@@ -0,0 +1,8 @@
+DROP SCHEMA IF EXISTS Accounts_db;
+CREATE SCHEMA IF NOT EXISTS Accounts_db;
+CREATE TABLE IF NOT EXISTS Accounts_db.Accounts (
+  `customerId` int NOT NULL,
+  `balance` decimal(10,2) NOT NULL,
+  PRIMARY KEY (`customerId`)
+);
+INSERT INTO Accounts_db.Accounts VALUES (1,'200.00');
