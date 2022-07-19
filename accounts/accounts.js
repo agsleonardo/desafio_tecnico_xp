859d7d599fd93185e3ea02ea809a80a8f6cf7d55
agsleonardo <agsleonardo@hotmail.com>
2022-07-19 15:45:32 -0300

    feat: metodo para saque ok

--- accounts/accounts.js
+++ accounts/accounts.js
@@ -4,10 +4,19 @@ const customError = require('./utils/error');
 const getByAccountId = async (req, res) => {
   const { accountId } = req.params;
   const accountData = await model.getByAccountId(accountId);
-  if (!accountData) throw customError(404, 'Conta não encontrada');
   res.status(200).send(accountData);
+};
+
+const withdraw = async (req, res) => {
+  const { accountId, amount } = req.body;
+  const accountData = await model.getByAccountId(accountId);
+  if (+accountData.balance < +amount) throw customError(400, 'Saldo insuficiente');
+  const newBalance = accountData.balance - amount;
+  await model.updateBalance(accountId, newBalance);
+  res.status(200).send({message: `Saque realizado com sucesso! Novo saldo: ${newBalance}`});
 };
 
 module.exports = {
   getByAccountId,
+  withdraw,
 };
