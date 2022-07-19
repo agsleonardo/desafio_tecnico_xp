81983e2406f56aad607d39adc7042145908412b8
agsleonardo <agsleonardo@hotmail.com>
2022-07-19 18:22:18 -0300

    refactor: alterando rotas dos microserviços para pegar variavel de ambiente

--- server.js
+++ server.js
@@ -1,20 +1,26 @@
 const httpProxy = require('express-http-proxy');
 const express = require('express');
 const app = express();
-var logger = require('morgan');
- 
+const logger = require('morgan');
+require('dotenv').config();
+
+const PORT = process.env.PORT || 10000;
+const AUTH_URL = process.env.AUTH_URL || 'http://localhost:3000/';
+const CUSTOMER_URL = process.env.CUSTOMER_URL || 'http://localhost:4000/';
+const ACCOUNTS_URL = process.env.ACCOUNTS_URL || 'http://localhost:5000/';
+
 app.use(logger('dev'));
 
-app.use('/auth', httpProxy('http://localhost:3000/'));
+app.use('/auth', httpProxy(AUTH_URL));
 
-app.use('/customers', httpProxy('http://localhost:4000/'));
+app.use('/customers', httpProxy(CUSTOMER_URL));
 
-app.use('/accounts', httpProxy('http://localhost:5000/'));
+app.use('/accounts', httpProxy(ACCOUNTS_URL));
 
-app.use('/*', (req, res) => res.status(404).send('Bad Route!'));
+app.use('/*', (_req, res) => res.status(404).send('Bad Route!'));
 
 app.use(express.json());
  
-app.listen(10000, () => {
+app.listen(PORT, () => {
     console.log('API Gateway running!');
 });
