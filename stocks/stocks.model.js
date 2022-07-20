6158cf8a758a7ff7baeb9d0428bfb34d4bec45a8
agsleonardo <agsleonardo@hotmail.com>
2022-07-20 13:06:31 -0300

    feat: iniciando model de stocks

---
+++ stocks/stocks.model.js
@@ -0,0 +1,45 @@
+const fs = require('fs');
+const path = require('path');
+
+const mysql = require('mysql2/promise');
+
+require('dotenv').config();
+
+const connection = mysql.createPool({
+  host: process.env.MYSQL_HOST || 'localhost',
+  user: process.env.MYSQL_USER || 'root',
+  password: process.env.MYSQL_PASSWORD || 'root',
+});
+
+async function recreateDatabase() {
+  try {
+    const importPath = path.resolve(__dirname, 'stocks.sql');
+    const seedDBContent = fs.readFileSync(importPath).toString();
+    const queries = seedDBContent.split(';').filter((p) => p.trim());
+    const createTable = queries.pop();
+    const queriesPromises = queries.map(async (query) => connection.query(query));
+    await Promise.all(queriesPromises)
+      .then(() => connection.query(createTable));
+    process.stdout.write('\nDatabase inciado...\n');
+    return '\nDatabase inciado...\n';
+  } catch (error) {
+    process.stdout.write(`\nFalha em restaurar o Banco Stocks. ${error}\n`);
+    return setTimeout(() => recreateDatabase(), 1000);
+  }
+}
+
+const getByStockId = async (customerId) => {
+  const [rows] = await connection.query('SELECT * FROM Stocks_db.Stocks WHERE stockId = ?', [customerId]);
+  return rows;
+};
+
+const getAll = async (customerId) => {
+  const [rows] = await connection.query('SELECT * FROM Stocks_db.Stocks', [customerId]);
+  return rows;
+};
+
+module.exports = {
+  getByStockId,
+  recreateDatabase,
+  getAll,
+};
