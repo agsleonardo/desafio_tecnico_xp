97c496f365ba44cd066b31bef7177241dd63eb24
agsleonardo <agsleonardo@hotmail.com>
2022-07-24 22:23:57 -0300

    refactor: ajuste rotina ordem de compra

--- orders/utils/stocks.js
+++ orders/utils/stocks.js
@@ -2,16 +2,16 @@ const Request = require('./request');
 
 require('dotenv').config();
 
-const STOCKS_URL = process.env.STOCKS_URL || 'http://localhost:7100';
+const STOCKS_URL = process.env.STOCKS_URL || 'http://localhost:7100/';
 
 const getStockInfoById = async (stockId) => {
-  const stockBroker = await Request.get(`${STOCKS_URL}/${stockId}`)
+  const stockBroker = await Request.get(`${STOCKS_URL}${stockId}`)
     .catch(() => null);
   return stockBroker;
 };
 
 const updateStock = async (stockId, stockQty) => {
-  const stockBroker = await Request.put(`${STOCKS_URL}/`, { stockId, stockQty })
+  const stockBroker = await Request.put(`${STOCKS_URL}`, { stockId, stockQty })
     .catch(() => null);
   return stockBroker;
 };
